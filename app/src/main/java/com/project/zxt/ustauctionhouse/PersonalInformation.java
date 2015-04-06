7b5c9f195e2c6638671e71826958f74d034becbe
Xutong <xzhouaf@connect.ust.hk>
2015-04-07 01:13:38 +0800

    me

--- app/src/main/java/com/project/zxt/ustauctionhouse/PersonalInformation.java
+++ app/src/main/java/com/project/zxt/ustauctionhouse/PersonalInformation.java
@@ -11,7 +11,7 @@ import android.widget.TextView;
  * Created by Xutong on 2015/4/6.
  */
 public class PersonalInformation extends Activity implements View.OnClickListener{
-    private static final String TAG = "LogIn";
+    private static final String TAG = "Personal Information";
     private LinearLayout personalInfo, myAuction, myBid, myHistory;
 
     protected void onCreate(Bundle savedInstanceState) {
@@ -24,7 +24,23 @@ public class PersonalInformation extends Activity implements View.OnClickListene
         personalInfo = (LinearLayout) findViewById(R.id.personal_info);
         personalInfo.setOnClickListener(this);
 
+        myAuction = (LinearLayout) findViewById(R.id.myAuction);
+        myAuction.setOnClickListener(this);
 
+        myBid = (LinearLayout) findViewById(R.id.myBid);
+        myBid.setOnClickListener(this);
+
+        myHistory = (LinearLayout) findViewById(R.id.myHistory);
+        myHistory.setOnClickListener(this);
+
+
+
+    }
+
+    @Override
+    public void onDestroy() {
+        Log.i(TAG, "onDestroy()");
+        super.onDestroy();
     }
 
     public void onClick(View v) {
